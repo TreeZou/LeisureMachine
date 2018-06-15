ebbbc2353a66dd2dd483a0cca5cc163610ce064e
JunZou <1052363946@qq.com>
2018-06-15 08:44:36 +0800

    测试合并代码

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { HomeServiceProvider } from '../../providers/home-service/home-service'
-import { LoadingService } from '../../services/loading/loading.service'
-import { ToastServiceProvider } from '../../providers/toast-service/toast-service'
+import { HomeServiceProvider } from '../../providers/home-service/home-service';
+import { LoadingService } from '../../services/loading/loading.service';
+import { ToastServiceProvider } from '../../providers/toast-service/toast-service';
+import { ToastMessageServiceProvider } from '../../providers/toast-message-service/toast-message-service'
 
 @Component({
   selector: 'page-home',
@@ -16,7 +17,8 @@ export class HomePage {
   constructor(public navCtrl: NavController, 
               private homeService: HomeServiceProvider, 
               private loadingService: LoadingService,
-              private toastService: ToastServiceProvider) {
+              private toastService: ToastServiceProvider,
+              private toastMessageService: ToastMessageServiceProvider) {
 
   }
 
@@ -24,5 +26,9 @@ export class HomePage {
     
   }
 
+
+  toSearch(): any {
+    this.toastMessageService.showTip("进入搜索页面", '2000', 'bottom');
+  }
 
 }
